9b4b4d558d24339eceeac23bd9cd5b9edb98eb38
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-12-05 19:13:56 +0900

    2023-12-05 > 간단한 가로 스크롤 - 이정욱

---
+++ src/components/chart/text/TextChart.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export const TextChart = () => {
+    return(
+        <View>
+            <Text>
+                텍스트 차트
+            </Text>
+        </View>
+    )
+}
--- src/screens/Chart.screen.js
+++ src/screens/Chart.screen.js
@@ -1,5 +1,41 @@
-const ChartScreen = () => {
+import { ScrollView,View, Text } from "react-native";
+import { CircleChart } from "../components/chart/circle/CircleChart";
+import { LineChart } from "../components/chart/line/LineChart";
+import { TextChart } from "../components/chart/text/TextChart";
 
+const ChartScreen = () => {
+    // 스택헤서 헤더 X
+    // 읽은 권수, 읽은 페이지 월 평균 (텍스트)
+    // 원 그래프로 장르 그래프 작성 (원)
+    // 월별 평균 독서 시간  (막대)
+    // 월마다 읽은 책 그래프 (꺾은선)
+    return(
+        <View>
+            <View>
+                <Text>
+                    차트 스크린
+                </Text>
+            </View>
+            <ScrollView
+                horizontal={true}
+                showHorizontalScrollIndicator={false}
+            >
+                <CircleChart />
+            </ScrollView>
+            <ScrollView
+                horizontal={true}
+                showHorizontalScrollIndicator={false}
+            >
+                <LineChart />
+            </ScrollView>
+            <ScrollView
+                horizontal={true}
+                showHorizontalScrollIndicator={false}
+            >
+                <TextChart />
+            </ScrollView>
+        </View>
+    )
 }
 
 export default ChartScreen;
